8619a36bde3d19853f599f4ebc779eb2445c2985
Владимир <_vchibunin@bk.ru>
2026-08-04 22:11:15 +0400

    Add logging to masks and utils

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,20 @@
 import json
+import logging
+from pathlib import Path
+
+logs_dir = Path(__file__).resolve().parent.parent / "logs"
+logs_dir.mkdir(exist_ok=True)
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+
+if not logger.handlers:
+    file_handler = logging.FileHandler(logs_dir / "utils.log", mode="w", encoding="utf-8")
+    formatter = logging.Formatter(
+        "%(asctime)s %(name)s %(levelname)s %(message)s"
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
 
 def load_transactions(file_path: str) -> list:
@@ -12,9 +28,16 @@ def load_transactions(file_path: str) -> list:
             data = json.load(file)
 
         if isinstance(data, list):
+            logger.info("Транзакции успешно загружены")
             return data
 
+        logger.error("JSON не содержит список")
         return []
 
-    except FileNotFoundError, json.JSONDecodeError:
+    except FileNotFoundError:
+        logger.error("Файл не найден")
+        return []
+
+    except json.JSONDecodeError:
+        logger.error("Ошибка чтения JSON")
         return []
